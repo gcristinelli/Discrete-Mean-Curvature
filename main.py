a074eb3c164457542a84f73e2b63148e51dce548
Giacomo Cristinelli <giacomo.cristinelli@gmail.com>
2023-11-07 23:52:29 +0100

    First loop of cuts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dolfin import *
 import maxflow
 import time as time
+import ufl
 from mshr import *
 from matplotlib import cm, pyplot as pp
 from datetime import datetime
@@ -29,20 +30,40 @@ def _main():
     random = True
     d = 2
     height = 0.2
+    input_slope = 1
+
+    # BOUNDARY PARAMETERS
+    face_coeff = 30
+    bdy_coeff_bone = -0.95
+    bdy_coeff_scaffold = -0.9
+    bdy_coeff_side = 1
+
+    # ELASTICITY Parameters
+    mu = 8.0  # Lame coefficient
+    lbd = 16.0  # Lame coefficient
+    eps1 = 0.0001  # small parameter avoiding division by zero in the normal computation
+    weak = 0.01  # multiplicative coefficient for the "weak" material mimicking void
+    e0 = 0.9  # spontaneous strain along bottom boundary
+    elas_coeff = 0.00  # coefficient in front of shape derivative for descent
 
     # GEOMETRY LOOP ----------------------------------------------------------------------------------------------------
     print("Starting geometry loop...\n")
     start = time.time()
 
     # B1.---MESH GENERATION AND FUNCTION SPACES
-    domain = create_mesh([[lx1, ly1, lz1], [lx2, ly2, lz2]], [nx, ny, n], random)
+    coord = [[lx1, ly1, lz1], [lx2, ly2, lz2]]
+    domain = create_mesh(coord, [nx, ny, n], random)
     plot(domain, linewidth=0.25)
     pp.savefig(rd + '/mesh.png', bbox_inches='tight', dpi=300)
     pp.close()
 
     V = FunctionSpace(domain, 'DG', 0)  # PWC
     VL = FunctionSpace(domain, 'CG', 1)  # PWL
-    vol_cells, adesivity = Function(V), Function(V)
+    V_vec = VectorFunctionSpace(domain, 'DG', 0)
+    Vmat = TensorFunctionSpace(domain, 'DG', 0)
+    VL_vec = VectorFunctionSpace(domain, 'CG', 1)
+
+    vol_cells, bdy_length = Function(V), Function(V)
     domain.init(d - 1, d)
     f2c = domain.topology()(d - 1, d)
     adjacency = np.empty(shape=[0, 2])
@@ -68,10 +89,10 @@ def _main():
     elif d == 3:
         facet_size = np.array([Face(domain, face).area() for face in facets_list])
 
-    # Adding boundary/adesivity info
+    # Adding boundary info
     for facet in bdy_facets:
         if mid_cell[f2c(facet)[0]][d - 1] >= height:
-            adesivity.vector()[f2c(facet)[0]] = +facet_size[facet]
+            bdy_length.vector()[f2c(facet)[0]] = +facet_size[facet]
 
     # B2.---GRAPH GENERATION, creating graph with (d-1)-facets areas/length as weights
     G = maxflow.GraphFloat()
@@ -80,6 +101,42 @@ def _main():
 
     print("Making the mesh of {} vertices, {} {}-dimensional cells, and the graph took - {} seconds \n".format(
         domain.num_vertices(), domain.num_cells(), d, time.time() - start))
+
+    # PDE AND INPUT ----------------------------------------------------------------------------------------------------
+    input_data = Function(V)
+    input_data.vector()[:] = np.array([input_fun(mid_cell[cell], coord, input_slope)
+                                       for cell in range(domain.num_cells())])
+    # Plotting the input
+    ax = plot(input_data, vmin=-1.0, vmax=1.0)
+    pp.savefig(rd + '/input.png', bbox_inches='tight', dpi=300)
+    pp.close()
+
+    # distance function to boundary of input
+    bdy_input = MeshFunction('size_t', domain, d - 1, 0)
+    start = time.time()
+    for facet in internal_facets:
+        bdy_input.array()[facet] = np.abs(input_data.vector()[f2c(facet)[0]] - input_data.vector()[f2c(facet)[1]])
+    for facet in bdy_facets:
+        bdy_input.array()[facet] = np.abs(input_data.vector()[f2c(facet)[0]])
+    print(time.time()-start)
+
+    #Fast marching method to solve Eikonal equation
+
+    # MAIN LOOP --------------------------------------------------------------------------------------------------------
+    max_it, it, stop = [50, 1, False]
+    cut_result = Function(V)
+    while it <= max_it or stop:
+        face_coeff = face_coeff * 0.8
+        print("--Doing iteration", it)
+        # L1.---Distance function to the boundary of the previous cut
+        bdy_cut = MeshFunction('size_t', domain, d - 1, 0)
+
+        # L2.--- new cut
+        cut_value = linear_problem(domain, G, face_coeff, input_data, cut_result, vol_cells, bdy_length)
+        ax = plot(cut_result, vmin=0.0, vmax=1.0)
+        pp.savefig(rd + '/cut_%s.png' % it, bbox_inches='tight', dpi=300)
+        pp.close()
+        it += 1
 
 
 ########################################################################################################################
--- operations.py
+++ operations.py
@@ -16,6 +16,14 @@ def result_directory():
     return rd
 
 
+def input_fun(x, coord, sl):
+    input_val = (sl * (np.abs(x[0] - coord[0][0])) + (x[1] - coord[1][1]) < 0.0) | \
+                ((x[0] - coord[1][0]) ** 2 + (
+                        x[1] - (coord[0][1] + 0.5 * (coord[1][1] - coord[0][1]))) ** 2 < np.minimum(
+                    0.5 * (coord[1][0] - coord[0][0]), 0.5 * (coord[1][1] - coord[0][1])) ** 2)
+    return - float(input_val)
+
+
 def create_mesh(coord, n, random):
     if random:
         here = Rectangle(Point(coord[0][0], coord[0][1]), Point(coord[1][0], coord[1][1]))
@@ -24,3 +32,45 @@ def create_mesh(coord, n, random):
         domain = RectangleMesh(Point(coord[0][0], coord[0][1]), Point(coord[1][0], coord[1][1]),
                                n[0], n[1], 'crossed')
     return domain
+
+
+def linear_problem(domain, graph, face_coeff, dist, cut_result, vol_cells, bdy_length):
+    val = face_coeff * dist.vector() * vol_cells.vector() + bdy_length.vector()
+    graph.add_grid_tedges(np.arange(domain.num_cells()), np.maximum(0.0, val), np.maximum(0.0, -val))
+    energy = graph.maxflow()
+    cut_result.vector()[:] = graph.get_grid_segments(np.arange(domain.num_cells())).astype(float)
+    graph.add_grid_tedges(np.arange(domain.num_cells()), -np.maximum(0.0, val), -np.maximum(0.0, -val))
+    return energy
+
+def _solve_elasticity_dirichlet(domain, cut, dx, coord, mu, lbd, weak, e0):
+    V = FunctionSpace(domain, 'DG', 0)  # PWC
+    V_vec = VectorFunctionSpace(domain, 'DG', 0)
+
+    def left(x, on_boundary): return x[0] < coord[0][0] + DOLFIN_EPS
+
+    def right(x, on_boundary): return x[0] > coord[1][0] - DOLFIN_EPS
+
+    def up(x, on_boundary): return x[1] > coord[0][1] - DOLFIN_EPS
+
+    def down(x, on_boundary): return x[1] < coord[1][1] + DOLFIN_EPS
+
+    mismatch = Expression(("-1.0 * E0 * ( x[0] - 0.5*(Lx2+Lx1) )", " 0.0"), E0=e0, Lx1=coord[0][0], Lx2=coord[1][0],
+                          degree=1)
+    bc3 = DirichletBC(V_vec, mismatch, down)
+    bcs = [bc3]
+    u, v = [TrialFunction(V_vec), TestFunction(V_vec)]
+    S1 = 2.0 * mu * inner(sym(grad(u)), sym(grad(v))) + lbd * div(u) * div(v)
+    a = S1 * weak * dx(0) + S1 * dx(1)
+    L = inner(Constant((0.0, 0.0)), v) * dx
+    A, b = assemble_system(a, L, bcs)
+    solver = LUSolver(A)
+    solver.solve(U.vector(), b)
+    SE = 2.0 * mu * inner(sym(grad(U)), sym(grad(U))) + lbd * div(U) * div(U)
+    energy = assemble(SE * weak * dx(0) + SE * dx(1))
+    edens_aux = Function(V)
+    edens_aux = project(SE, V)
+    edens = Function(V)
+    edens_vec = np.multiply(edens_aux.vector(), cut.vector()) + \
+                weak * np.multiply(edens_aux.vector(), 1.0 - cut.vector())
+    edens.vector()[:] = edens_vec
+    return edens, energy
